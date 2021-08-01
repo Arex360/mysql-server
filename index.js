6901aaff9ed641d47e4ff083d66f238c646bef5a
Arex360 <49303647+Arex360@users.noreply.github.com>
2021-08-01 06:59:21 +0500

    ss

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ app.post('/findbyid',(req,response)=>{
     connection.end()
 })
 
-
+app.get('/',(req,res)=>{
+    res.send("hello")
+})
 app.listen(3000,()=>{
     console.log('server started')
     
