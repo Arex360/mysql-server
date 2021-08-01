c8b134a945c7b88616630d3f5f405bf811e8ea20
Arex360 <49303647+Arex360@users.noreply.github.com>
2021-08-01 06:55:43 +0500

    test3

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cors = require('cors')
+const bodyparser = require('body-parser')
 const mysql = require('mysql')
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -8,12 +10,20 @@ const connection = mysql.createConnection({
 })
 
 const app = express()
+app.use(cors())
+app.use(bodyparser.json())
+app.post('/findbyid',(req,response)=>{
+    let id = parseInt(req.body.id)
+    connection.connect()
+    connection.query(`select * from students where id = ${id}`,(err,res,field)=>{
+        console.log(res)
+        response.send(res)
+    })
+    connection.end()
+})
+
 
 app.listen(3000,()=>{
     console.log('server started')
-    connection.connect()
-    console.log('connected to database')
-    connection.query('select * from students',(err,results,fields)=>{
-        console.log(results)
-    })
+    
 })
