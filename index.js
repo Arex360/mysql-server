4c50a32583da1133fe6497e28e075bb5c249eabb
Arex360 <49303647+Arex360@users.noreply.github.com>
2021-08-01 06:49:05 +0500

    test

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express')
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'arex',
+    password: '123',
+    database: 'student'
+})
+
 const app = express()
+
 app.listen(3000,()=>{
     console.log('server started')
+    connection.connect()
+    console.log('connected to database')
 })
